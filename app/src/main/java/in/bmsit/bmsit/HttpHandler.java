73729424e9df2d8ab516ba633f5f38cc10d59e29
Mayank Arora <mickeydudester@gmail.com>
2014-09-22 18:28:56 +0530

    Authentication working now
    Fixed the authentication. The app receives the token and authenticates
    the user. Just a dummy user with credentials "asdf & asdf".

---
+++ app/src/main/java/in/bmsit/bmsit/HttpHandler.java
@@ -0,0 +1,7 @@
+package in.bmsit.bmsit;
+
+/**
+ * Created by Mayank on 20-09-2014.
+ */
+public class HttpHandler {
+}
--- app/src/main/java/in/bmsit/bmsit/Login.java
+++ app/src/main/java/in/bmsit/bmsit/Login.java
@@ -1,17 +1,32 @@
 package in.bmsit.bmsit;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class Login extends ActionBarActivity {
-
+    String TAG="BMSIT APP",ACCESS_TOKEN;
+    String id,password;
+    String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,13 +38,18 @@ public class Login extends ActionBarActivity {
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(eId.getText().toString().equals("1234")){
-                    Intent i = new Intent(".Broadcast");
-                    startActivity(i);
+                ConnectivityManager connectivityManager= (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+                if(networkInfo!=null && networkInfo.isConnected()){
+                    id=eId.getText().toString();
+                    password=ePass.getText().toString();
+                    url="http://192.168.0.100:4567/token?user_id="+id+"&password="+password;
+                    new Authenticate().execute(url);
                 }
                 else{
-                    Intent i = new Intent(".Home");
-                    startActivity(i);
+                    Toast.makeText(getApplicationContext(),"Not connected to the network",
+                            Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -42,8 +62,6 @@ public class Login extends ActionBarActivity {
         });
 
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -61,5 +79,50 @@ public class Login extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    private class Authenticate extends AsyncTask<String,Void,String>{
+
+        @Override
+        protected String doInBackground(String... urls) {
+            try {
+                return Access(urls[0]);
+            } catch (IOException e) {
+                return "IOException while accessing the URL";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            ACCESS_TOKEN=s;
+            Toast.makeText(getApplicationContext(),ACCESS_TOKEN,Toast.LENGTH_SHORT).show();
+        }
+
+        private String Access(String myUrl) throws IOException{
+            InputStream is = null;
+            try {
+                Log.d(TAG,"Entered Access method");
+                URL url = new URL(myUrl);
+                Log.d(TAG,url.toString());
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setReadTimeout(4000);
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+                int response = httpURLConnection.getResponseCode();
+                is = httpURLConnection.getInputStream();
+                Reader reader = null;
+                reader = new InputStreamReader(is, "UTF-8");
+                char[] buffer = new char[500];
+                reader.read(buffer);
+                String content = new String(buffer);
+                return content;
+            }
+            finally {
+                if(is!=null)
+                    is.close();
+            }
+
+        }
     }
 }
